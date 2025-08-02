b14983a1b787f91b4d0031e6dd739686917874f6
topshenyi-web <erxu@ctemc.org>
2025-08-02 14:31:56 -0400

    fix: port is a string

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -5,7 +5,7 @@ const envSchema = z.object({
   GITHUB_APP_PRIVATE_KEY: z.string(),
   GITHUB_APP_ID: z.coerce.number(),
   GITHUB_APP_INSTALLATION_ID: z.coerce.number(),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
 });
 const env = envSchema.parse(process.env);
 export default env;
