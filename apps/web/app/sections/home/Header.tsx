88258c6a244d35a6d0938989f5e2e32a5023c9d4
topshenyi-web <erxu@ctemc.org>
2025-08-30 18:33:14 -0400

    style: cleanup hearder css and styling

--- apps/web/app/sections/home/Header.tsx
+++ apps/web/app/sections/home/Header.tsx
@@ -6,11 +6,12 @@ import teamIcon from "../../assets/icons/team.svg";
 import projectsIcon from "../../assets/icons/projects.svg";
 import eventsIcon from "../../assets/icons/events.svg";
 import sponsorsIcon from "../../assets/icons/sponsors.svg";
-import hamburgerIcon from "../../assets/icons/hamburger.svg";
-import closeIcon from "../../assets/icons/close.svg";
+import hamburgerIcon from "../../assets/icons/hamburger.svg?inline";
+import closeIcon from "../../assets/icons/close.svg?inline";
 import { NavLink, useLocation } from "react-router";
-import { useCallback, useState } from "react";
-import { useIsMobile } from "../../hooks/useIsMobile";
+import { useEffect, useState } from "react";
+import { motion } from "motion/react";
+
 const navLinks = [
   {
     icon: projectsIcon,
@@ -34,90 +35,26 @@ const navLinks = [
   },
 ];
 function Header() {
-  const isMobile = useIsMobile();
+  const [menuOpen, setMenuIsOpen] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setMenuIsOpen(false);
+  }, [location]);
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleHamburger = useCallback(() => {
-    setIsOpen((prev) => !prev);
-  }, []);
-
-  return isMobile ? (
+  return (
     <header
       className={clsx(
         css["main-header-container"],
-        isMobile && isOpen && css["main-header-container-mobile"],
+        menuOpen && css["main-header-container--mobile-menu-open"],
       )}
     >
       <div className={css["main-header-layout"]}>
-        <NavLink
-          className={css["logo"]}
-          to={"/"}
-          // onClick={() =>
-          //   window.scrollTo({
-          //     top: 0,
-          //     left: 0,
-          //     behavior: "smooth",
-          //   })
-          // }
-          prefetch="intent"
-        >
+        <NavLink className={css["logo"]} to={"/"} prefetch="intent">
           <img className={css["logo__img"]} src={scottylabsLogo} />
 
           <div className={css["logo__text"]}>ScottyLabs</div>
         </NavLink>
-        <img
-          src={isOpen ? closeIcon : hamburgerIcon}
-          className={css["hamburger-icon"]}
-          onClick={toggleHamburger}
-        />
-      </div>
-      {isOpen && (
-        <div className={css["hamburger-menu"]}>
-          {navLinks.map(({ icon, url, text }, i) => (
-            <>
-              <NavLink
-                className={({ isActive }) =>
-                  clsx(
-                    css["nav-button-mobile"],
-                    isActive && css["nav-button--active"],
-                  )
-                }
-                to={url}
-                prefetch="intent"
-                key={url}
-              >
-                <img className={css["nav-button__icon"]} src={icon} />
-                <div className={css["nav-button__text"]}>{text}</div>
-              </NavLink>
-              {i === navLinks.length - 1 ? null : (
-                <hr className={css["hamburger-divider"]} />
-              )}
-            </>
-          ))}
-        </div>
-      )}
-    </header>
-  ) : (
-    <header className={css["main-header-container"]}>
-      <div className={css["main-header-layout"]}>
-        <NavLink
-          className={css["logo"]}
-          to={"/"}
-          // onClick={() =>
-          //   window.scrollTo({
-          //     top: 0,
-          //     left: 0,
-          //     behavior: "smooth",
-          //   })
-          // }
-          prefetch="intent"
-        >
-          <img className={css["logo__img"]} src={scottylabsLogo} />
-
-          <div className={css["logo__text"]}>ScottyLabs</div>
-        </NavLink>
-        <nav className={css["main-nav"]}>
+        <nav className={css["main-nav--desktop"]}>
           {navLinks.map(({ icon, url, text }) => (
             <NavLink
               className={({ isActive }) =>
@@ -132,7 +69,48 @@ function Header() {
             </NavLink>
           ))}
         </nav>
+        <img
+          src={menuOpen ? closeIcon : hamburgerIcon}
+          className={css["main-nav--mobile"]}
+          onClick={() => {
+            setMenuIsOpen((prev) => !prev);
+          }}
+        />
       </div>
+
+      {/* mobile hamburger menu, expanded */}
+      <motion.div
+        animate={{ height: menuOpen ? "auto" : 0 }}
+        style={{ overflow: "hidden" }}
+        transition={
+          menuOpen
+            ? {
+                type: "spring",
+                damping: 15,
+                stiffness: 150,
+              }
+            : {}
+        }
+      >
+        <div className={css["hamburger-menu"]}>
+          {navLinks.map(({ icon, url, text }) => (
+            <NavLink
+              className={({ isActive }) =>
+                clsx(
+                  css["nav-button-mobile"],
+                  isActive && css["nav-button-mobile--active"],
+                )
+              }
+              to={url}
+              prefetch="render"
+              key={url}
+            >
+              <img src={icon} />
+              <div>{text}</div>
+            </NavLink>
+          ))}
+        </div>
+      </motion.div>
     </header>
   );
 }
