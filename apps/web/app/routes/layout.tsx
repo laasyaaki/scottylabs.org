f802e35e43f67dbe56e5891dbef47ed0d72010ca
topshenyi-web <erxu@ctemc.org>
2025-09-05 17:13:53 -0400

    chore: revert viewport styling

--- apps/web/app/routes/layout.tsx
+++ apps/web/app/routes/layout.tsx
@@ -5,10 +5,6 @@ import Footer from "../sections/home/Footer";
 export default function Layout() {
   return (
     <div style={{ minWidth: 950 }}>
-      {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
-      {/* <meta name="viewport" content="width=1524" /> */}
-      <meta name="viewport" content="width=device-width, initial-scale=.3" />
-      {/* https://stackoverflow.com/questions/31334481/how-to-force-desktop-view-on-mobile-devices */}
       <Header />
 
       <main>
