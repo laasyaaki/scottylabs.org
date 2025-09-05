2ab5dde135f4fef51553c03d1ce2cbeb739e6bd0
topshenyi-web <erxu@ctemc.org>
2025-09-05 17:25:23 -0400

    fix: use standard viewport scaling on clerk pages

--- apps/web/app/routes/layout.tsx
+++ apps/web/app/routes/layout.tsx
@@ -6,7 +6,10 @@ export default function Layout() {
   return (
     <div style={{ minWidth: 950 }}>
       <Header />
-
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+      {/* <meta name="viewport" content="width=1524" /> */}
+      <meta name="viewport" content="width=device-width, initial-scale=.3" />
+      {/* https://stackoverflow.com/questions/31334481/how-to-force-desktop-view-on-mobile-devices */}
       <main>
         <Outlet />
       </main>
