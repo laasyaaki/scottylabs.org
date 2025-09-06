ce520fa655a4adf2806e762ad53c665169fa16b9
topshenyi-web <erxu@ctemc.org>
2025-09-06 19:05:38 -0400

    chore: inline all sponsor logos

--- apps/web/app/sections/sponsors/sponsors.ts
+++ apps/web/app/sections/sponsors/sponsors.ts
@@ -1,18 +1,18 @@
-import accenture from "../../assets/sponsors-page/sponsors/accenture.svg";
-import agentuity from "../../assets/sponsors-page/sponsors/agentuity.svg";
-import balysany from "../../assets/sponsors-page/sponsors/balysany.svg";
-import citadel from "../../assets/sponsors-page/sponsors/citadel.svg";
-import coderabbit from "../../assets/sponsors-page/sponsors/coderabbit.svg";
-import commvault from "../../assets/sponsors-page/sponsors/commvault.svg";
-import deshaw from "../../assets/sponsors-page/sponsors/deshaw.svg";
-import fly from "../../assets/sponsors-page/sponsors/fly.svg";
-import hrt from "../../assets/sponsors-page/sponsors/hrt.svg";
-import janeStreet from "../../assets/sponsors-page/sponsors/janestreet.svg";
-import modal from "../../assets/sponsors-page/sponsors/modal.svg";
-import optiver from "../../assets/sponsors-page/sponsors/optiver.svg";
-import scale from "../../assets/sponsors-page/sponsors/scale.svg";
-import snl from "../../assets/sponsors-page/sponsors/snl.svg";
-import visa from "../../assets/sponsors-page/sponsors/visa.svg";
+import accenture from "../../assets/sponsors-page/sponsors/accenture.svg?inline";
+import agentuity from "../../assets/sponsors-page/sponsors/agentuity.svg?inline";
+import balysany from "../../assets/sponsors-page/sponsors/balysany.svg?inline";
+import citadel from "../../assets/sponsors-page/sponsors/citadel.svg?inline";
+import coderabbit from "../../assets/sponsors-page/sponsors/coderabbit.svg?inline";
+import commvault from "../../assets/sponsors-page/sponsors/commvault.svg?inline";
+import deshaw from "../../assets/sponsors-page/sponsors/deshaw.svg?inline";
+import fly from "../../assets/sponsors-page/sponsors/fly.svg?inline";
+import hrt from "../../assets/sponsors-page/sponsors/hrt.svg?inline";
+import janeStreet from "../../assets/sponsors-page/sponsors/janestreet.svg?inline";
+import modal from "../../assets/sponsors-page/sponsors/modal.svg?inline";
+import optiver from "../../assets/sponsors-page/sponsors/optiver.svg?inline";
+import scale from "../../assets/sponsors-page/sponsors/scale.svg?inline";
+import snl from "../../assets/sponsors-page/sponsors/snl.svg?inline";
+import visa from "../../assets/sponsors-page/sponsors/visa.svg?inline";
 
 export type SponsorTypes = "lowest" | "partner" | "premier";
 
